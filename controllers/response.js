4fd093a0fe0865dd549c80af5916c96b15a9de02
Olexandr <olex@teeko.io>
2020-05-17 15:36:30 +0300

    wip on eosgate implementation

--- controllers/response.js
+++
@@ -1,6 +0,0 @@
-const eosService = require('../services/eos.service');
-
-exports.add_response = function (req, res) {
-    eosService.add_response();
-    res.send('NOT IMPLEMENTED: Add response');
-};
--- loaders/index.js
+++
@@ -1,6 +0,0 @@
-const expressLoader = require('./express');
-
-module.exports.init = async function init({ expressApp }) {
-    await expressLoader({ app: expressApp });
-    console.log('Express Intialized');
-}
