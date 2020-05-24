828ac372c5de2b2ab1c0946092dfa84c7bf2c294
Artem <artem.v.maluga@gmail.com>
2020-05-25 00:50:46 +0300

    trying forward result

--- controllers/form.js
+++ controllers/form.js
@@ -3,21 +3,19 @@ const eosService = require('../services/eos.service');
 module.exports.create_form = function (req, res) {
     console.info('\nREQUEST:', req.method, req.originalUrl, req.body);
     eosService.createForm(req.body).then(function (result) {
-        res.body = result;
-        res.status(200).end();
+        res.status(200);
         console.info('\nRESPONSE:', res.statusCode, res.statusMessage);
-        console.info(res.body);
-        res.send();
+        console.info(result);
+        res.send(result);
     });
 };
 
 module.exports.delete_form = function (req, res) {
     console.info('\nREQUEST:', req.method, req.originalUrl, req.body);
     eosService.deleteForm(req.body).then(function (result) {
-        res.body = result;
-        res.status(200).end();
+        res.status(200);
         console.info('\nRESPONSE:', res.statusCode, res.statusMessage);
-        console.info(res.body);
-        res.send();
+        console.info(result);
+        res.send(result);
     });
 };
--- controllers/response.js
+++ controllers/response.js
@@ -3,10 +3,9 @@ const eosService = require('../services/eos.service');
 module.exports.add_response = function (req, res) {
     console.info('\nREQUEST:', req.method, req.originalUrl, req.body);
     eosService.addResponse(req.body).then(function (result) {
-        res.body = result;
-        res.status(200).end();
+        res.status(200);
         console.info('\nRESPONSE:', res.statusCode, res.statusMessage);
         console.info(res.body);
-        res.send();
+        res.send(result);
     });
 };
